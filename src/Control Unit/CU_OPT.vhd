59c00ce5ca15515599e4bbbf786da18d07bb79db
Safan <AbdallahSafan05@gmail.com>
2025-12-10 19:47:42 +0200

    handle more optimization

--- src/Control Unit/CU_OPT.vhd
+++ src/Control Unit/CU_OPT.vhd
@@ -56,7 +56,13 @@ BEGIN
   -----------------------------------------------------------------------------
   -- CONTROL SIGNALS
   -----------------------------------------------------------------------------
-
+ if OP_CODE = "00000" then
+    -- immediate, leave the signals as it was
+    read_next_inst <= '1';
+    rd_en <= '0';
+    imm_sig <= '0';
+else
+    
   -- # Decode Stage
   -- Read Next Instruction:
   -- Stop for: IADD (wait for imm), LDM/LDD/STD (mem access), Branch/Jumps/CALL (control hazard)
@@ -72,10 +78,10 @@ BEGIN
 
   -- # Execute Stage
   -- ALU Source: IADD (10 - Reg/Imm) vs ADD/Others (01 - Reg/Reg) vs INC (00)
-  ALU_SRC <= "10" WHEN is_iadd ELSE 
+  ALU_SRC <= "10" WHEN  is_iadd OR (is_group_10 AND sub_bits(2) = '1') ELSE -- LDM, LDD, STD m IADD 
              "00" WHEN (is_group_00 AND sub_bits = "100") ELSE -- INC
              "01"; -- Default (ADD, SUB, AND, SWAP)
-
+  
   -- ALU Opcode Mapping (Bit 2,1,0 patterns)
   -- NOT(101), MOV/PUSH(110), SWAP(111), ADD/IADD/INC(001), SUB(010), AND(100)
   ALU_OP <= "101" WHEN (is_group_00 AND sub_bits = "011") ELSE -- NOT
@@ -99,38 +105,43 @@ BEGIN
 
   PC_WE     <= '0' WHEN OP_CODE = "00001" ELSE '1'; -- HLT
   OUT_EN    <= '1' WHEN OP_CODE = "00101" ELSE '0'; -- OUT
-  IMM_SIG   <= '1' WHEN is_iadd OR OP_CODE = "10010" ELSE '0'; -- IADD or LDM
+--   IMM_SIG   <= '1' WHEN is_iadd OR OP_CODE = "10010" ELSE '0'; -- IADD or LDM
 
-  -- Stack Pointer: RTI(11), RET/POP(10), PUSH/CALL/INT(01)
-  SP_OP     <= "11" WHEN OP_CODE = "11011" ELSE -- RTI
-               "10" WHEN OP_CODE = "11001" OR OP_CODE = "10001" ELSE -- RET, POP
-               "01" WHEN OP_CODE = "10000" OR OP_CODE = "11000" OR OP_CODE = "11010" ELSE -- PUSH, CALL, INT
+  SP_OP     <= "01" WHEN OP_CODE = "11011" OR OP_CODE = "11001" OR OP_CODE = "10001" ELSE -- RTI , RET , POP
+               "11" WHEN OP_CODE = "10000" OR OP_CODE = "11000" OR OP_CODE = "11010" ELSE -- PUSH, CALL, INT
                "00";
 
   -- # Memory Stage
-  PC_SEL      <= '1' WHEN is_ret_rti ELSE '0'; -- RET, RTI
+  PC_SEL      <= '1' WHEN OP_CODE = "11010" ELSE '0'; -- INT
 
   -- Write Enable: PUSH, STD, CALL, INT
   MEM_WRT_EN  <= '1' WHEN OP_CODE="10000" OR OP_CODE="10100" OR OP_CODE="11000" OR OP_CODE="11010" ELSE '0';
 
   -- Address: SP(10) for Stack ops/Int/Call, Data(01) for LDD/STD
-  MEM_ADDR    <= "10" WHEN is_group_11 OR (is_group_10 AND sub_bits(1 downto 0) /= "10" AND sub_bits(1 downto 0) /= "11") ELSE
-                 "01" WHEN OP_CODE = "10011" OR OP_CODE = "10100" ELSE -- LDD, STD
-                 "00";
+  MEM_ADDR    <= "01" WHEN OP_CODE = "10000" OR OP_CODE = "11000" OR OP_CODE = "11010" ELSE -- call , int , push
+                 "11" WHEN OP_CODE = "10011" OR OP_CODE = "10100" ELSE -- LDD , STD
+                 "10";
 
+
+  -- ////////////////////////////////////////////////////////
+  -- TODO: handle push flags 
+  -- ////////////////////////////////////////////////////////
   -- Write Data: INT(10), PUSH/STD(01), CALL(00)
-  MEM_WRT_DATA <= "10" WHEN OP_CODE = "11010" ELSE
-                  "01" WHEN OP_CODE = "10000" OR OP_CODE = "10100" ELSE
+  MEM_WRT_DATA <= "10" WHEN OP_CODE = "10000" OR OP_CODE = "10100" ELSE
                   "00";
 
   -- # Write Back Stage
   -- Data: MOV/SWAP(11), LDM(10), ALU/IN(01), LDD/POP(00)
-  WB_DATA     <= "11" WHEN is_group_01 AND (sub_bits = "000" OR sub_bits = "001") ELSE -- MOV, SWAP
-                 "10" WHEN OP_CODE = "10010" ELSE -- LDM
-                 "01" WHEN is_group_01 OR OP_CODE="00011" OR OP_CODE="00100" OR OP_CODE="00110" ELSE -- ALU, NOT, INC, IN
-                 "00";
+  -- ////////////////////////////////////////////////////////
+  -- TODO: handle swap instruction WB_DATA
+  -- ////////////////////////////////////////////////////////
+  WB_DATA     <= "01" WHEN OP_CODE = "00110" ELSE -- IN
+                 "10" WHEN is_group_10 AND (sub_bits = "001" or sub_bits = "011") ELSE
+                 "00"; 
 
-  WB_ADDR     <= "00"; -- Fixed
+  WB_ADDR     <= "01" WHEN OP_CODE = "10011" OR (is_group_10 AND (sub_bits="000" OR sub_bits="001" OR sub_bits="101")) ELSE 
+                 "10" WHEN is_group_10 AND (sub_bits="010" OR sub_bits="011" OR sub_bits="100") ELSE
+                 "00"; -- Fixed
   
   -- Reg Write: Group 01 (All), Group 00 (NOT, INC, IN), Group 10 (POP, LDM, LDD)
   REG_WRT_EN  <= '1' WHEN is_group_01 OR 
@@ -138,6 +149,6 @@ BEGIN
                           (is_group_10 AND (sub_bits="001" OR sub_bits="010" OR sub_bits="011"))
                      ELSE '0';
 
-  SWAP_SIG    <= '1' WHEN OP_CODE = "01001" ELSE '0';
-
+  SWAP_SIG    <= '1' WHEN OP_CODE = "01001" ELSE '0'; -- SWAP
+END IF;
 END ARCHITECTURE;
